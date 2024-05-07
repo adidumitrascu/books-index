80c58ab635f0e78dfb471fffc198bd3f2ab564f5
adidumitrascu <adrian.mihai.dumitrascu@gmail.com>
2024-05-07 10:48:40 +0200

    init author routes

--- routes/authors.js
+++ routes/authors.js
@@ -4,8 +4,21 @@ const Author = require('../models/author')
 
 //All author route
 
-router.get('/', (req, res) => {
-    res.render('authors/index')
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', { 
+            authors: authors, 
+            searchOptions: req.query 
+          })
+    } catch {
+        res.redirect('/')
+    }
 })
 
 //New author route
@@ -22,7 +35,7 @@ router.post('/', async (req, res) => {
     })
     try {
       const newAuthor = await author.save()
-      res.redirect(`authors/${newAuthor.id}`)
+      res.redirect('authors')
     } catch {
       res.render('authors/new', {
         author: author,
@@ -30,9 +43,6 @@ router.post('/', async (req, res) => {
       })
     }
   })
-
-
-
 
 
 
